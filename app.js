acd189c4d4a933ad394eb90d328ee671908c510b
Mo101991 <94375721+Mo101991@users.noreply.github.com>
2026-09-09 14:12:59 -0700

    Website-Dateien hochladen

---
+++ app.js
@@ -0,0 +1,16 @@
+(function () {
+  var toggle = document.querySelector(".menu-toggle");
+  var links = document.querySelector(".nav-links");
+  if (toggle && links) {
+    toggle.addEventListener("click", function () {
+      var open = links.classList.toggle("open");
+      toggle.setAttribute("aria-expanded", open ? "true" : "false");
+    });
+    links.querySelectorAll("a").forEach(function (a) {
+      a.addEventListener("click", function () {
+        links.classList.remove("open");
+        toggle.setAttribute("aria-expanded", "false");
+      });
+    });
+  }
+})();
